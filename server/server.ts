49a4e93582bd5707f17d485ed446329361d9f4de
freawee <abdouobaid@hotmail.com>
2025-02-14 15:22:00 +0100

    initital

--- server/server.ts
+++ server/server.ts
@@ -46,7 +46,7 @@ app.get("/api", async (req: Request, res: Response): Promise<void> => {
 });
 
 // Écoute sur le port 5000
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+  console.log(`Serveur en cours d'exécution sur ${PORT}`);
 });
